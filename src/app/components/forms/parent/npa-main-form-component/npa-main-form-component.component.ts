98a5933ef5e3c9b288ff125aeef03df2316365f7
ravichanderjha <ravichanderjha@gmail.com>
2026-01-20 15:57:30 +0530

    fix warning

--- src/app/components/forms/parent/npa-main-form-component/npa-main-form-component.component.ts
+++ src/app/components/forms/parent/npa-main-form-component/npa-main-form-component.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgComponentOutlet } from '@angular/common';
 import { ComponentLoadService } from '../../../../services/component-load.service';
 
 @Component({
   selector: 'app-npa-main-form-component',
-  imports: [],
+  imports: [NgComponentOutlet],
   templateUrl: './npa-main-form-component.component.html',
   styleUrl: './npa-main-form-component.component.css'
 })
