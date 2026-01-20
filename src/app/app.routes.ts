ed6515327027fa50b89c6b79fd4e672cd2faf910
ravichanderjha <ravichanderjha@gmail.com>
2026-01-20 12:57:57 +0530

    login works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,12 @@ import { LoginComponent } from './components/auth/login/login.component';
 
 export const routes: Routes = [
     {
-        path: '',component: LoginComponent
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
     }
- 
 ];
--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../../auth/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    this.authService.login({ 
+      username: this.username, 
+      password: this.password 
+    }).subscribe({
+      next: (response: any) => {
+        console.log('Role:', response.role);
+        console.log('Token:', response.token);
+        alert('Login successful!');
+      },
+      error: (error: any) => {
+        console.error('Login failed:', error);
+        alert('Login failed!');
+      }
+    });
+  }
 }
